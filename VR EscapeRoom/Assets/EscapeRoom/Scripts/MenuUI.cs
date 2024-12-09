63661817fa6db277872d37a6c3e15b6e96849279
KacperWojdak <kacper.wojdak@interia.pl>
2024-12-09 15:25:37 +0100

    Base game room setup

--- VR EscapeRoom/Assets/EscapeRoom/Scripts/MenuUI.cs
+++ VR EscapeRoom/Assets/EscapeRoom/Scripts/MenuUI.cs
@@ -5,7 +5,7 @@ public class MenuUI : MonoBehaviour
 {
     public void StartGame()
     {
-        SceneManager.LoadScene("DemoScene");
+        SceneManager.LoadScene("GameScene");
     }
 
     public void ExitGame()
